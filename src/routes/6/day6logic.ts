c92b15bfe33096b8801970b91d97e2f231dffff7
Stefan Krüger <stefan-krueger@hotmail.de>
2022-12-06 13:52:35 +0100

    add performance tracking to day 6

--- src/routes/6/day6logic.ts
+++ src/routes/6/day6logic.ts
@@ -5,6 +5,8 @@ import {readData} from '../../lib/parser/parser';
 export const calculateMagic = () => {
     const data: string[] = readData(day6realData)[0].split('');
 
+    const perf = performance.now();
+
     let foundIndex = 0;
 
     forEach(data, (_, aIndex) => {
@@ -16,6 +18,7 @@ export const calculateMagic = () => {
     })
 
     console.log('index: ', foundIndex);
+    console.log('runtime: ', performance.now() - perf);
 
     return {
         one: foundIndex,
